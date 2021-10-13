90d83b745ad60049728ad47ab65f9ad63282edb0
Hemanth <trhemanth2@gmail.com>
2021-10-13 21:26:14 +0530

    rendering markup

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,19 @@ import express from 'express';
 const app = express();
 
 app.get('*', (req, res) => {
-    res.send('hello world');
+    const markup = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title></title>
+        </head>
+        <body>
+            <div id="react">Hello world!</div>
+        </body>
+    </html>
+    `;
+
+    res.send(markup);
 });
 
 app.listen(3000, 'localhost');
